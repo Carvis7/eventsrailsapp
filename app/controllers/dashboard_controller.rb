38b8c890d9ddb5ac1b7456fbf32c53e765c8ba46
Carvis7 <carlydavis91@gmail.com>
2020-02-15 15:11:06 +1100

    added devise dashboard

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,11 @@
+class DashboardController < ApplicationController
+  before_action :authenticate_user!
+
+
+  def index
+    @events = Event.where(user_id: current_user.id)
+    @attendances = Attendance.where(user_id: current_user.id)
+  end
+
+
+end
